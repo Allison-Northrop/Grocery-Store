8b38d7c6267bb855af148ad861b92d2e73e3d4ca
Allison Northrop <agnorthrop@gmail.com>
2017-04-02 23:43:53 -0700

    first method for aisle passing tests

--- specs/aisle_spec.rb
+++ specs/aisle_spec.rb
@@ -1,1 +1,19 @@
 require_relative 'spec_helper'
+
+describe "aisle class" do
+
+  it "requires one parameter" do
+    category = "BAKING"
+
+    aisle = StockManager::Aisle.new(category)
+
+    aisle.must_respond_to :category
+    aisle.category.must_equal category 
+  end
+
+
+
+
+
+
+end
